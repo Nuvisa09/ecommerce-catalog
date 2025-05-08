eea684dcffd2d5d6e6d986fa315711903f853356
Nuvisa09 <nurnisa0922@gmail.com>
2025-05-08 22:16:38 +0700

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -25,52 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-=======
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
->>>>>>> 921246f4f52787921f4c1f731e7132f8718b84b4
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/main.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-=======
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
->>>>>>> 921246f4f52787921f4c1f731e7132f8718b84b4
